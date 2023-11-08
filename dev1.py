46cdc5694a705989155bc56c5594237347d3cb48
lukfernandes <luccademagalhaes@gmail.com>
2023-11-08 15:22:10 -0300

    primeira e segunda função

--- dev1.py
+++ dev1.py
@@ -1,1 +1,43 @@
-print('Lucca Fernandes')
+import random
+
+def filtra (listapal, numletras):
+
+    listafinal = []
+
+    for palavra in listapal:
+
+        palavramin = palavra.lower()
+
+        palavra.strip()
+
+        if len(palavramin) == numletras:
+
+            if palavramin not in listafinal:
+
+                listafinal.append(palavramin)
+    
+    return listafinal
+
+
+def inicializa (listapal):
+
+    dicfinal = {}
+
+    for palavra in listapal:
+
+        numletras = len(palavra)
+
+        tentativa = numletras + 1
+
+        
+    sorteada = random.choice(listapal)
+
+    dicfinal['n'] = numletras
+
+    dicfinal['sorteada'] = sorteada
+
+    dicfinal['especuladas'] = []
+
+    dicfinal['tentativas'] = numletras + 1
+
+    return dicfinal
