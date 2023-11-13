94874d53d91477ceea66f2cb9945133a3bfc268c
morgana0carvalho <otomemari@gmail.com]>
2023-11-12 23:50:45 -0300

    FINAL

--- funcoes.py
+++ funcoes.py
@@ -1,201 +1,222 @@
 import random
 from palavras import PALAVRAS as plvs
 
-def filtra (listapal, numletras):
+while True:
+    def filtra (listapal, numletras):
 
-    listafinal = []
+        listafinal = []
 
-    for palavra in listapal:
+        for palavra in listapal:
 
-        palavramin = palavra.lower()
+            palavramin = palavra.lower()
 
-        palavra.strip()
+            palavra.strip()
 
-        if len(palavramin) == numletras:
+            if len(palavramin) == numletras:
 
-            if palavramin not in listafinal:
+                if palavramin not in listafinal:
 
-                listafinal.append(palavramin)
-    
-    return listafinal
-
-listaatualizada = filtra(plvs, 5)
-
-def inicializa (listapal):
-
-    dicfinal = {}
-
-    for palavra in listapal:
-
-        numletras = len(palavra)
-
-        tentativa = numletras + 1
-
+                    listafinal.append(palavramin)
         
-    sorteada = random.choice(listapal)
+        return listafinal
 
-    dicfinal['n'] = numletras
+    listaatualizada = filtra(plvs, 5)
 
-    dicfinal['sorteada'] = sorteada
+    def inicializa (listapal):
 
-    dicfinal['especuladas'] = []
+        dicfinal = {}
 
-    dicfinal['tentativas'] = numletras + 1
+        for palavra in listapal:
 
-    return dicfinal
+            numletras = len(palavra)
 
-dicionario = inicializa(listaatualizada)
+            tentativa = numletras + 1
 
-sorteada = dicionario['sorteada']
+            
+        sorteada = random.choice(listapal)
 
-def inidica_posicao(sorteada, especulada):
-    if len(sorteada) != len(especulada):
-        return []
-    resultado = []
-    for i in range(len(sorteada)):
-        if sorteada[i] == especulada[i]:
-            resultado.append(0)
-        elif especulada[i] in sorteada:
-            resultado.append(1)
-        else:
-            resultado.append(2)
+        dicfinal['n'] = numletras
 
-    return resultado
+        dicfinal['sorteada'] = sorteada
 
+        dicfinal['especuladas'] = []
 
+        dicfinal['tentativas'] = numletras + 1
 
+        return dicfinal
 
+    dicionario = inicializa(listaatualizada)
 
-#PRINTS
+    sorteada = dicionario['sorteada']
 
-print('============================')
-print('|                          |')
-print('|  Bem-vindoS ao Termovita |')
-print('|                          |')
-print('=====Design de Software=====')
-print('')
-print('')
-print('Comandos: Desisto')
-print('')
-print('REGRAS:')
-print('')
-print(' - Você tem 6 tentativas para acertar uma palavra aleatória de 5 letras.')
-print(' - A cada tentativa, a palavra escolhida terá suas letras coloridas conforme:')
-print('')
-print(' \033[94m   . Azul \033[30m : a letra está na posição correta;')
-print(' \033[93m   . Amarelo \033[30m : a palavra tem a letra, mas está na posição errada;')
-print(' \033[90m   . Cinza \033[30m : a palavra não tem a letra.')
-print('')
-print(' - Os acentos são ignorados;')
-print(' - As palavras podem possuir letras repetidas.')
-print('')
-print('')
+    def inidica_posicao(sorteada, especulada):
+        if len(sorteada) != len(especulada):
+            return []
+        resultado = []
+        for i in range(len(sorteada)):
+            if sorteada[i] == especulada[i]:
+                resultado.append(0)
+            elif especulada[i] in sorteada:
+                resultado.append(1)
+            else:
+                resultado.append(2)
 
-
-
-print('Sorteando uma palavra...')
-print('Já tenho uma palavra, tente descobrir!' )
-
-#CORES PARA O TERMO
-cor_azul = "\033[94m"
-cor_preta = "\033[30m"
-cor_cinza = "\033[90m"
-cor_amarela = "\033[93m"
-
-
-
-def Tabela(lista_final, maxx, detalhes):
-    largura_celula = 3  
-    separacao = '-' * ((largura_celula + 1) * maxx + 1) + '\n'
-    tabela = separacao
-
-    for plvs in lista_final:
-        traco = '|'
-        for i in plvs:
-            traco += f'{i:^{largura_celula}}'
-        tabela += traco + '|\n' + separacao
-
-    if len(lista_final) < maxx:
-        tabela += f'{("|" + "  " * largura_celula) * (maxx - len(lista_final))} |' + '\n' + separacao
-
-    return tabela
+        return resultado
 
 
 
 
 
+    #PRINTS
+
+    print('============================')
+    print('|                          |')
+    print('|  Bem-vindoS ao Termovita |')
+    print('|                          |')
+    print('=====Design de Software=====')
+    print('')
+    print('')
+    print('Comandos: Desisto')
+    print('')
+    print('REGRAS:')
+    print('')
+    print(' - Você tem 6 tentativas para acertar uma palavra aleatória de 5 letras.')
+    print(' - A cada tentativa, a palavra escolhida terá suas letras coloridas conforme:')
+    print('')
+    print(' \033[94m   . Azul \033[30m : a letra está na posição correta;')
+    print(' \033[93m   . Amarelo \033[30m : a palavra tem a letra, mas está na posição errada;')
+    print(' \033[90m   . Cinza \033[30m : a palavra não tem a letra.')
+    print('')
+    print(' - Os acentos são ignorados;')
+    print(' - As palavras podem possuir letras repetidas.')
+    print('')
+    print('')
 
 
-a = 0
 
-lista_final = []
+    print('Sorteando uma palavra...')
+    print('Já tenho uma palavra, tente descobrir!' )
 
-i = 0
+    #CORES PARA O TERMO
+    cor_azul = "\033[94m"
+    cor_preta = "\033[30m"
+    cor_cinza = "\033[90m"
+    cor_amarela = "\033[93m"
 
-tenta = 6
 
-tabelao = ''
 
-while tenta != 0:
+    def Tabela(lista_final, maxx, detalhes):
+        largura_celula = 3
+        separacao = '-' * ((largura_celula + 1) * maxx + 1) + '\n'
+        tabela = separacao
 
-    print('Você tem {0} tentativa(s)'.format(tenta))
+        for plvs in lista_final:
+            traco = '|'
+            for i in plvs:
+                traco += f'{i:^{largura_celula}}|'
+            tabela += traco + '\n' + separacao
 
-    especulada = input('Qual o seu palpite?')
+        if len(lista_final) < maxx:
+            tabela += f'{("|" + " " * largura_celula) * len(plvs)}|' + '\n' + separacao
 
-    print('Insper :: Termovita')
-    
-    if especulada == sorteada:
-        a = 1
-        break
+        return tabela
 
-    elif especulada == 'Desisto':
 
-        break
-    
-    posicao = inidica_posicao(sorteada, especulada)
+    a = 0
 
-    palavra_colorida = []
+    lista_final = []
 
     i = 0
+
+    tenta = 6
+
+    palavras_tentadas = []
+
+    tabelao = ''
+
+    while tenta != 0:
+
+        print('Você tem {0} tentativa(s)'.format(tenta))
+
+        especulada = input('Qual o seu palpite?')
+
+        print('Insper :: Termovita')
+        
+        if especulada == sorteada:
+            a = 1
+            break
+
+        elif especulada == 'Desisto':
+
+            break
+
+        if especulada not in plvs:
+            print('não está nos meus dados, escreva outra ;)')
+            continue
+        if especulada in palavras_tentadas:
+            print('Palavra já testada, tente outra :)')
+            continue
+        
+
+        palavras_tentadas.append(especulada)
+
+        tenta -= 1
+        
+        posicao = inidica_posicao(sorteada, especulada)
+
+        palavra_colorida = []
+
+        i = 0
+        
+        for numero in posicao:
+
+            if  numero == 0 :
+                azul= cor_azul + especulada[i] + cor_cinza
+
+                palavra_colorida.append(azul)
+
+            elif numero == 1:
+
+                amarelo = cor_amarela + especulada[i] + cor_cinza
+            
+                palavra_colorida.append(amarelo)
+            
+            elif numero == 2:
+            
+                palavra_colorida.append(especulada[i])
+            
+            i += 1
+
+        lista_final.append (palavra_colorida)
+
+        if especulada in lista_final:
+            tenta +=1
+            
+        tabela = Tabela(lista_final, 6, '--- --- ---' )
+
+        if len(especulada) == 5:
+            print(tabela)
+        
+        else:
+            print('A palavra deve conter 5 letras!')
+            tenta += 1
+
+        
+
+    if a == 1:
+        print('Parabens!!')
+
+    else: 
+        print ("Poxa, não foi desta vez...")   
+
+    print('A palvra era...{0}'.format(sorteada))
+
+    final=input('Deseja continuar?(S/N)')
+    if final == 'N':
+        break
+
+
     
-    for numero in posicao:
-
-        if  numero == 0 :
-            azul= cor_azul + especulada[i] + cor_cinza
-
-            palavra_colorida.append(azul)
-
-        elif numero == 1:
-
-            amarelo = cor_amarela + especulada[i] + cor_cinza
-        
-            palavra_colorida.append(amarelo)
-        
-        elif numero == 2:
-        
-            palavra_colorida.append(especulada[i])
-        
-        i += 1
-
-    lista_final.append (palavra_colorida)
-
-    tabela = Tabela(lista_final, 6, '--- --- --- ---' )
-
-    if len(especulada) == 5:
-        print(tabela)
-    
-    else:
-        print('A palavra deve conter 5 letras!')
-        tenta += 1
-
-    tenta -= 1
-
-if a == 1:
-   print('Parabens!!')
-
-else: print ("Poxa, não foi desta vez...")   
-
-print('A palvra era...{0}'.format(sorteada))
 
 
 
